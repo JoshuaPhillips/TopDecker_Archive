a9ebf722b331419cfa06f4149dba9dafc0f15879
J-B-Phillips <joshuaphillips@monzo.com>
2019-12-06 16:33:22 +0000

    convert database_uri to env variable

--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  uri: `mongodb+srv://admin:9ehRZdjaA4zfhOg7@cluster0-m79gu.mongodb.net/topdecker_mrk2?retryWrites=true`,
+  uri: process.env.DATABASE_URI,
   config: {
     useNewUrlParser: true,
     useCreateIndex: true,
