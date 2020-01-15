70c84754b0e07a23cebc91f8dc5faf270772162c
JoshuaPhillips <jphillips961@gmail.com>
2020-01-15 16:28:43 +0000

    re-baseline cardList sorts by alphabetical ordr

--- client/src/utils/sortCardList.js
+++ client/src/utils/sortCardList.js
@@ -1,99 +1,60 @@
-export const sortCardList = (cardList, sortMode) => {
-  let sortedCardList = cardList;
-  switch (sortMode) {
-    case 'alphabetical':
-      sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
-        if (cardA.name < cardB.name) {
-          return -1;
-        }
+const sortAlphabetically = cardList => {
+  let sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
+    if (cardA.name < cardB.name) {
+      return -1;
+    }
 
-        if (cardA.name === cardB.name) {
-          return 0;
-        } else {
-          return 1;
-        }
-      });
-      break;
+    if (cardA.name === cardB.name) {
+      return 0;
+    } else {
+      return 1;
+    }
+  });
 
-    case 'cmc':
-      sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
-        const cmcDifference = cardA.cmc - cardB.cmc;
+  return sortedCardList;
+};
 
-        if (cmcDifference === 0) {
-          if (cardA.name < cardB.name) {
-            return -1;
-          }
+const sortByCmc = cardList => {
+  let sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
+    const cmcDifference = cardA.cmc - cardB.cmc;
 
-          if (cardA.name === cardB.name) {
-            return 0;
-          } else {
-            return 1;
-          }
-        } else {
-          return cmcDifference;
-        }
-      });
-      break;
+    if (cmcDifference === 0) {
+      if (cardA.name < cardB.name) {
+        return -1;
+      }
 
-    case 'color':
-      const colors = ['W', 'U', 'B', 'R', 'G'];
-
-      sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
-        // monocolored cards
-        if (cardA.colors.length === 1) {
-          if (cardB.colors.length !== 1) {
-            return -1;
-          } else {
-            // sort by WUBRG
-            const colorsIndexDifference =
-              colors.findIndex(color => {
-                return color === cardA.colors[0];
-              }) -
-              colors.findIndex(color => {
-                return color === cardB.colors[0];
-              });
-
-            if (colorsIndexDifference === 0) {
-              if (cardA.name < cardB.name) {
-                return -1;
-              }
-
-              if (cardA.name === cardB.name) {
-                return 0;
-              } else {
-                return 1;
-              }
-            }
-
-            return colorsIndexDifference;
-          }
-        }
-
-        // multicolored cards
-        if (cardA.colors.length > 1) {
-          if (cardB.colors.length === 0) {
-            return -1;
-          } else {
-            return 0;
-          }
-        }
-
+      if (cardA.name === cardB.name) {
         return 0;
-      });
-      break;
+      } else {
+        return 1;
+      }
+    } else {
+      return cmcDifference;
+    }
+  });
 
-    case 'rarity':
-      const rarities = ['common', 'uncommon', 'rare', 'mythic'];
-      sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
-        const rarityIndexDifference =
-          rarities.findIndex(rarity => {
-            return rarity === cardA.rarity;
+  return sortedCardList;
+};
+
+const sortByColour = cardList => {
+  const colors = ["W", "U", "B", "R", "G"];
+
+  let sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
+    // monocolored cards
+    if (cardA.colors.length === 1) {
+      if (cardB.colors.length !== 1) {
+        return -1;
+      } else {
+        // sort by WUBRG
+        const colorsIndexDifference =
+          colors.findIndex(color => {
+            return color === cardA.colors[0];
           }) -
-          rarities.findIndex(rarity => {
-            return rarity === cardB.rarity;
+          colors.findIndex(color => {
+            return color === cardB.colors[0];
           });
 
-        if (rarityIndexDifference === 0) {
+        if (colorsIndexDifference === 0) {
           if (cardA.name < cardB.name) {
             return -1;
           }
@@ -105,76 +66,140 @@ export const sortCardList = (cardList, sortMode) => {
           }
         }
 
-        return rarityIndexDifference;
+        return colorsIndexDifference;
+      }
+    }
+
+    // multicolored cards
+    if (cardA.colors.length > 1) {
+      if (cardB.colors.length === 0) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+
+    return 0;
+  });
+
+  return sortedCardList;
+};
+
+const sortByRarity = cardList => {
+  const rarities = ["common", "uncommon", "rare", "mythic"];
+
+  let sortedCardList = cardList.sort(({ card: cardA }, { card: cardB }) => {
+    const rarityIndexDifference =
+      rarities.findIndex(rarity => {
+        return rarity === cardA.rarity;
+      }) -
+      rarities.findIndex(rarity => {
+        return rarity === cardB.rarity;
       });
+
+    if (rarityIndexDifference === 0) {
+      if (cardA.name < cardB.name) {
+        return -1;
+      }
+
+      if (cardA.name === cardB.name) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+
+    return rarityIndexDifference;
+  });
+
+  return sortedCardList;
+};
+
+const sortByType = cardList => {
+  const types = ["Creature", "Planeswalker", "Artifact", "Enchantment", "Instant", "Sorcery", "Land"];
+  let cardTypeMatchMap = {
+    creature: [],
+    planeswalker: [],
+    artifact: [],
+    enchantment: [],
+    instant: [],
+    sorcery: [],
+    land: []
+  };
+
+  for (var j = 0; j < cardList.length; j++) {
+    // for each card, split that card's typeline into words
+    const typeLineWordArray = cardList[j].card.type_line.split(" ");
+
+    for (var i = 0; i < typeLineWordArray.length; i++) {
+      // for each of those words, check if it's included in the 'types' array above
+      const matchingTypeIndex = types.indexOf(typeLineWordArray[i]);
+
+      if (matchingTypeIndex !== -1) {
+        if (typeLineWordArray[i] === "Artifact" && typeLineWordArray[i + 1] === "Creature") {
+          cardTypeMatchMap.creature.push(cardList[j]);
+          break;
+        } else if (typeLineWordArray[i] === "Enchantment" && typeLineWordArray[i + 1] === "Creature") {
+          cardTypeMatchMap.creature.push(cardList[j]);
+          break;
+        } else {
+          cardTypeMatchMap[types[matchingTypeIndex].toLowerCase()].push(cardList[j]);
+          break;
+        }
+      }
+    }
+  }
+
+  for (var k = 0; k < Object.keys(cardTypeMatchMap).length; k++) {
+    cardTypeMatchMap[Object.keys(cardTypeMatchMap)[k]].sort(({ card: cardA }, { card: cardB }) => {
+      if (cardA.name < cardB.name) {
+        return -1;
+      }
+
+      if (cardA.name === cardB.name) {
+        return 0;
+      } else {
+        return 1;
+      }
+    });
+  }
+
+  const result = [
+    ...cardTypeMatchMap.creature,
+    ...cardTypeMatchMap.planeswalker,
+    ...cardTypeMatchMap.artifact,
+    ...cardTypeMatchMap.enchantment,
+    ...cardTypeMatchMap.instant,
+    ...cardTypeMatchMap.sorcery,
+    ...cardTypeMatchMap.land
+  ];
+
+  return result;
+};
+
+export const sortCardList = (cardList, sortMode) => {
+  let alphabatizedCardList = sortAlphabetically(cardList);
+
+  switch (sortMode) {
+    case "cmc":
+      alphabatizedCardList = sortByCmc(alphabatizedCardList);
       break;
 
-    case 'type':
-      const types = ['Creature', 'Planeswalker', 'Artifact', 'Enchantment', 'Instant', 'Sorcery', 'Land'];
-      let cardTypeMatchMap = {
-        creature: [],
-        planeswalker: [],
-        artifact: [],
-        enchantment: [],
-        instant: [],
-        sorcery: [],
-        land: []
-      };
+    case "color":
+      alphabatizedCardList = sortByColour(alphabatizedCardList);
+      break;
 
-      for (var j = 0; j < cardList.length; j++) {
-        // for each card, split that card's typeline into words
-        const typeLineWordArray = cardList[j].card.type_line.split(' ');
+    case "rarity":
+      alphabatizedCardList = sortByRarity(alphabatizedCardList);
+      break;
 
-        for (var i = 0; i < typeLineWordArray.length; i++) {
-          // for each of those words, check if it's included in the 'types' array above
-          const matchingTypeIndex = types.indexOf(typeLineWordArray[i]);
-
-          if (matchingTypeIndex !== -1) {
-            if (typeLineWordArray[i] === 'Artifact' && typeLineWordArray[i + 1] === 'Creature') {
-              cardTypeMatchMap.creature.push(cardList[j]);
-              break;
-            } else if (typeLineWordArray[i] === 'Enchantment' && typeLineWordArray[i + 1] === 'Creature') {
-              cardTypeMatchMap.creature.push(cardList[j]);
-              break;
-            } else {
-              cardTypeMatchMap[types[matchingTypeIndex].toLowerCase()].push(cardList[j]);
-              break;
-            }
-          }
-        }
-      }
-
-      for (var k = 0; k < Object.keys(cardTypeMatchMap).length; k++) {
-        cardTypeMatchMap[Object.keys(cardTypeMatchMap)[k]].sort(({ card: cardA }, { card: cardB }) => {
-          if (cardA.name < cardB.name) {
-            return -1;
-          }
-
-          if (cardA.name === cardB.name) {
-            return 0;
-          } else {
-            return 1;
-          }
-        });
-      }
-
-      const result = [
-        ...cardTypeMatchMap.creature,
-        ...cardTypeMatchMap.planeswalker,
-        ...cardTypeMatchMap.artifact,
-        ...cardTypeMatchMap.enchantment,
-        ...cardTypeMatchMap.instant,
-        ...cardTypeMatchMap.sorcery,
-        ...cardTypeMatchMap.land
-      ];
-
-      sortedCardList = result;
-
+    case "type":
+      alphabatizedCardList = sortByType(alphabatizedCardList);
       break;
 
     default:
       break;
   }
 
-  return sortedCardList;
+  return alphabatizedCardList;
 };
